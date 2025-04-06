4b71f59d64a7897b03e366f9f64112e14c1f6d00
ZeLebo <sartakov.sasha@icloud.com>
2025-04-06 11:02:31 +0700

    changes

--- main.py
+++ main.py
@@ -1,25 +1,8 @@
 from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
-
-
-class EncryptRequest(BaseModel):
-    text: str
-    key: int
-
-class DecryptRequest(BaseModel):
-    text: str
-
 from fastapi import FastAPI, HTTPException
 import requests
 
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Разрешить запросы с любого источника (для разработки)
-    allow_credentials=True,
-    allow_methods=["*"],  # Разрешить все методы (GET, POST и т.д.)
-    allow_headers=["*"],  # Разрешить любые заголовки
-)
 
 dictionary = set()
 
@@ -44,7 +27,7 @@ def caesar_cipher(text: str, key: int, mode: str = "encrypt") -> str:
             offset = (ord(char) - base + key) % 26 if mode == 'encrypt' else (ord(char) - base - key) % 26
             result.append(chr(base + offset))
         else:
-            result.append(char)  # Не изменяем неалфавитные символы
+            result.append(char)
     return ''.join(result)
 
 def is_meaningful(text: str) -> bool:
@@ -72,17 +55,6 @@ def startup_event():
         print("Dictionary loaded successfully!")
     except Exception as e:
         print(f"Error loading dictionary: {e}")
-
-@app.post("/encrypt")
-def encrypt_post(request: EncryptRequest):
-    return {"encrypted_text": caesar_cipher(request.text, request.key)}
-
-@app.post("/decrypt")
-def decrypt_post(request: DecryptRequest):
-    key, decrypted_text = find_key_with_dictionary(request.text)
-    if key is None:
-        raise HTTPException(status_code=400, detail="Could not decrypt the text")
-    return {"key": key, "decrypted_text": decrypted_text}
 
 @app.get("/encrypt")
 def encrypt(text: str, key: int):
